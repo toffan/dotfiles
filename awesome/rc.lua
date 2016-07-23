8520abd617cee8890df6dc2e8e0fed88955918c8
toffan <benoit.lemarchand@inpt.fr>
2016-07-24 00:00:51 +0200

    Clean libraries

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -1,16 +1,16 @@
 -- Required libraries {{{
 -- Standard awesome library
-local gears = require("gears")
-local awful = require("awful")
-awful.rules = require("awful.rules")
-require("awful.autofocus")
+local gears     = require("gears")
+local awful     = require("awful")
+awful.rules     = require("awful.rules")
+                  require("awful.autofocus")
 -- Widget and layout library
-local wibox = require("wibox")
+local wibox     = require("wibox")
 -- Theme handling library
 local beautiful = require("beautiful")
 -- Notification library
-local naughty = require("naughty")
-local menubar = require("menubar")
+local naughty   = require("naughty")
+local menubar   = require("menubar")
 -- Vicious
 local vicious = require("vicious")
 -- }}}
